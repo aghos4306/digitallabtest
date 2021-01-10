c80e969aacfad9fd70f99a7471251b9445dad643
Aghogho Akpedeye <aghogho.akpedeye@gmail.com>
2021-01-10 14:15:28 +0200

    products dropdown logic

---
+++ src/components/MenuItems.js
@@ -0,0 +1,28 @@
+export const MenuItems = [
+    {
+        title: 'Fruit',
+        path: '/fruit',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'Vegetables',
+        path: '/vegetable',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'Eggs',
+        path: '/eggs',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'Milk',
+        path: '/milk',
+        cName: 'dropdown-link'
+    },
+    {
+        title: 'Fresh Drinks',
+        path: '/fresh-drinks',
+        cName: 'dropdown-link'
+    }
+
+]
